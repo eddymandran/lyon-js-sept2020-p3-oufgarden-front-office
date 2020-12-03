f9042a8b049d5e87489dda43a9e63b90f9ba1f6b
Nathan-Vanstaevel <nathanvanstaevel@gmail.com>
2020-12-03 15:15:23 +0100

    navbar component with css and route

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './styles/Navbar.scss';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className='menu'>
+        <ul>
+          <Link to='/articles'>
+            <div className='articles' />
+          </Link>
+          <Link to='/calendar'>
+            <div className='calendar' />
+          </Link>
+          <Link to='/garden'>
+            <div className='jardin' />
+          </Link>
+          <Link to='/user'>
+            <div className='user' />
+          </Link>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
