c786c21d3d0a42f291fcf5ebfcc4ccce42e69f33
Pella0 <pellattiero.ju@gmail.com>
2020-12-08 19:51:54 +0100

    suppression des commentaires, des fichiers inutiles + css

--- src/components/LoginF.js
+++ src/components/LoginF.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import "./styles/Login.scss";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import './styles/Login.scss';
 
-const required = "This field is required";
+const required = 'This field is required';
 
 const errorMessage = (error) => {
-  return <div className="invalid-feedback">{error}</div>;
+  return <div className='invalid-feedback'>{error}</div>;
 };
 
 const LoginF = () => {
@@ -13,98 +13,45 @@ const LoginF = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="container">
-      <div className="box">
-        <h3>Client Login</h3>
-
-        <div className="col-sm-12">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="form-group">
-              <div className="input-wrapper">
-                <div className="field">
-                  <input
-                    className="form-control"
-                    type="email"
-                    placeholder="Email"
-                    name="Email"
-                    ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-                  />
-                  {errors.Email &&
-                    errors.Email.type === "required" &&
-                    errorMessage(required)}
-                </div>
-                <div className="form-group">
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Password"
-                    name="Password"
-                    ref={register({ required: true })}
-                  />
-                  {errors.Password &&
-                    errors.Password.type === "required" &&
-                    errorMessage(required)}
-                </div>
-
-                <div className="form-group">
-                  <button type="submit" className="button">
-                    Login
-                  </button>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
-  /* const { register, handleSubmit, handleChange, errors } = useForm();
-
-  return (
     <div className='container'>
       <div className='box'>
-        <form onSubmit={handleSubmit}>
+        <h3>Client Login</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className='input-wrapper'>
             <div className='field'>
-              <label className='label'>Email Address</label>
-              <div className='control'>
-                <input
-                  className='form-control'
-                  type='email'
-                  placeholder='Email'
-                  name='Email'
-                  ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-                />
-                {errors.Email &&
-                  errors.Email.type === 'required' &&
-                  errorMessage(required)}
-              </div>
-            </div>
-            <div className='field'>
-              <label className='label'>Password</label>
-              <div className='control'>
-                <input
-                  className='form-control'
-                  type='password'
-                  placeholder='Password'
-                  name='Password'
-                  onChange={handleChange}
-                  ref={register({ required: true })}
-                />
-                {errors.Password &&
-                  errors.Password.type === 'required' &&
-                  errorMessage(required)}
-              </div>
-            </div>
+              <input
+                className='form-control'
+                type='email'
+                placeholder='Email'
+                name='Email'
+                ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+              />
+              {errors.Email &&
+                errors.Email.type === 'required' &&
+                errorMessage(required)}
 
-            <button type='submit' className='button'>
-              Login
-            </button>
+              <input
+                className='form-control'
+                type='password'
+                placeholder='Password'
+                name='Password'
+                ref={register({ required: true })}
+              />
+              {errors.Password &&
+                errors.Password.type === 'required' &&
+                errorMessage(required)}
+
+              <div className='form-group'>
+                <button type='submit' className='button'>
+                  Login
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </div>
     </div>
-  ); */
+  );
 };
 
 export default LoginF;
