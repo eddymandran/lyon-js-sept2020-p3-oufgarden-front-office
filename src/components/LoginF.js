3bf0f5124e463b60e2995c37fdd3a825cc484d06
Pella0 <pellattiero.ju@gmail.com>
2020-12-10 14:56:25 +0100

    login similaire au login backOffice, meme systeme d'appel au back end

--- src/components/LoginF.js
+++ src/components/LoginF.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import "./styles/Login.scss";
+import "./style/Login.scss";
+import axios from "axios";
 
-const required = "This field is required";
+require("dotenv").config();
+
+const required = "Ce champs est requis";
 
 const errorMessage = (error) => {
   return <div className="invalid-feedback">{error}</div>;
 };
 
-const LoginF = () => {
+const LoginF = (props) => {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+
+  const url = process.env.REACT_APP_API_BASE_URL;
+
+  const onSubmit = (data) => {
+    axios
+      .post(`${url}/login`, data)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === "logged") {
+          props.history.push("/adherents");
+          setIsLogged(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <div className="container">
       <div className="box">
-        <h3>Client Login</h3>
+        <h3>Log in</h3>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="input-wrapper">
             <div className="field">
               <input
+                onChange={(event) => setEmail(event.target.value)}
+                value={email}
                 className="form-control"
                 type="email"
                 placeholder="Email"
-                name="Email"
+                name="email"
                 ref={register({ required: true, pattern: /^\S+@\S+$/i })}
               />
               {errors.Email &&
@@ -31,10 +55,12 @@ const LoginF = () => {
                 errorMessage(required)}
 
               <input
+                onChange={(event) => setPassword(event.target.value)}
+                value={password}
                 className="form-control"
                 type="password"
                 placeholder="Password"
-                name="Password"
+                name="password"
                 ref={register({ required: true })}
               />
               {errors.Password &&
@@ -43,7 +69,7 @@ const LoginF = () => {
 
               <div className="form-group">
                 <button type="submit" className="button">
-                  Login
+                  connexion
                 </button>
               </div>
             </div>
