59734f6b4fd28ceca7eb80f300b89db2612badf7
Pella0 <pellattiero.ju@gmail.com>
2021-01-04 18:58:59 +0100

    ajout Login similaire Back-office, css au figma, en attente de cabler correctement pour authentification

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 /* import { useQuery } from 'react-query'; */
-import { useForm } from 'react-hook-form';
-import './style/Login.scss';
-import { useToasts } from 'react-toast-notifications';
+import { useForm } from "react-hook-form";
+import "./style/Login.scss";
+import { useToasts } from "react-toast-notifications";
 
-import API from '../services/API';
+import API from "../services/API";
 // import { UserContext } from './_context/UserContext';
 
 /* require('dotenv').config(); */
@@ -14,79 +14,79 @@ const Login = (props) => {
   /* const { setIsAdmin } = useContext(UserContext);
    */
   const { register, handleSubmit, errors } = useForm();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   // eslint-disable-next-line no-unused-vars
   const [isLogged, setIsLogged] = useState(false);
   const [stayConnected, setStayConnected] = useState(false);
-  const required = 'Ce champs est requis';
+  const required = "Ce champs est requis";
 
   const errorMessage = (error) => {
-    return <div className='invalid-feedback'>{error}</div>;
+    return <div className="invalid-feedback">{error}</div>;
   };
 
   const onSubmit = (data) => {
     console.log(data);
-    API.post('/login', data)
+    API.post("/login", data)
       .then((res) => {
-        if (res.data === 'logged') {
+        if (res.data === "logged") {
           // setIsAdmin(true);
           setIsLogged(true);
-          addToast('logged in successfully', {
-            appearance: 'success',
+          addToast("logged in successfully", {
+            appearance: "success",
             autoDismiss: true,
           });
-          props.history.push('/feed');
+          props.history.push("/feed");
         }
       })
       .catch((err) => {
         console.log(err);
-        addToast('Vos acces ne sont pas valides', {
-          appearance: 'error',
+        addToast("Vos acces ne sont pas valides", {
+          appearance: "error",
           autoDismiss: true,
         });
       });
   };
 
   return (
-    <div className='containerLogin'>
-      <div className='logo'></div>
-      <div className='boxLogin'>
+    <div className="containerLogin">
+      <div className="logo"></div>
+      <div className="boxLogin">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className='field'>
+          <div className="field">
             <input
               onChange={(event) => setEmail(event.target.value)}
               value={email}
-              className='form-control'
-              type='email'
-              placeholder='Email'
-              name='email'
+              className="form-control"
+              type="email"
+              placeholder="Email"
+              name="email"
               ref={register({ required: true, pattern: /^\S+@\S+$/i })}
             />
             {errors.Email &&
-              errors.Email.type === 'required' &&
+              errors.Email.type === "required" &&
               errorMessage(required)}
 
             <input
               onChange={(event) => setPassword(event.target.value)}
               value={password}
-              className='form-control'
-              type='password'
-              placeholder='Mot de passe'
-              name='password'
+              className="form-control"
+              type="password"
+              placeholder="Mot de passe"
+              name="password"
               ref={register({ required: true })}
             />
             {errors.Password &&
-              errors.Password.type === 'required' &&
+              errors.Password.type === "required" &&
               errorMessage(required)}
-            <div className='stay-connected-container'>
-              <div className='stay-connected'>
-                <label htmlFor='stayConnected'>
+            <div className="stay-connected-container">
+              <div className="stay-connected">
+                <label htmlFor="stayConnected">
                   <input
                     ref={register}
-                    name='stayConnected'
-                    id='stayConnected'
-                    type='checkbox'
+                    name="stayConnected"
+                    id="stayConnected"
+                    type="checkbox"
                     value={stayConnected}
                     onClick={() => setStayConnected(true)}
                   />
@@ -95,11 +95,11 @@ const Login = (props) => {
               </div>
             </div>
 
-            <div className='form-group'>
-              <button type='submit' className='button'>
+            <div className="form-group">
+              <button type="submit" className="button">
                 connexion
               </button>
-              <div className='infoCreation'>
+              <div className="infoCreation">
                 <p>
                   La création de votre espace peut prendre jusqu'à 72h à compter
                   de la réception de l'adhésion.
