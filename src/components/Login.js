ed76202e5752ad932dd133236e251a0e0ded266e
Pella0 <pellattiero.ju@gmail.com>
2021-01-05 18:46:41 +0100

    merge du login pour pouvoir acceder au feed sans erreur 401, ajout de balise dans le return article pour visibilité des elements importés de la database article

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 /* import { useQuery } from 'react-query'; */
-import { useForm } from "react-hook-form";
-import "./style/Login.scss";
-import { useToasts } from "react-toast-notifications";
+import { useForm } from 'react-hook-form';
+import './style/Login.scss';
+import { useToasts } from 'react-toast-notifications';
 
-import API from "../services/API";
+import API from '../services/API';
 // import { UserContext } from './_context/UserContext';
 
 /* require('dotenv').config(); */
@@ -14,13 +14,13 @@ const Login = (props) => {
   /* const { setIsAdmin } = useContext(UserContext);
    */
   const { register, handleSubmit, errors } = useForm();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   // eslint-disable-next-line no-unused-vars
   const [isLogged, setIsLogged] = useState(false);
   const [stayConnected, setStayConnected] = useState(false);
-  const required = "Veuillez saisir une adresse e-mail valide";
-  const requiredPassword = "Veuillez saisir votre mot de passe";
+  const required = 'Veuillez saisir une adresse e-mail valide';
+  const requiredPassword = 'Veuillez saisir votre mot de passe';
 
   const errorMessage = (error) => {
     return <div className="invalid-feedback">{error}</div>;
@@ -28,22 +28,22 @@ const Login = (props) => {
 
   const onSubmit = (data) => {
     console.log(data);
-    API.post("/login", data)
+    API.post('/login', data)
       .then((res) => {
-        if (res.data === "logged") {
+        if (res.data === 'logged') {
           // setIsAdmin(true);
           setIsLogged(true);
-          addToast("logged in successfully", {
-            appearance: "success",
+          addToast('logged in successfully', {
+            appearance: 'success',
             autoDismiss: true,
           });
-          props.history.push("/feed");
+          props.history.push('/feed');
         }
       })
       .catch((err) => {
         console.log(err);
-        addToast("Vos acces ne sont pas valides", {
-          appearance: "error",
+        addToast('Vos acces ne sont pas valides', {
+          appearance: 'error',
           autoDismiss: true,
         });
       });
@@ -51,7 +51,7 @@ const Login = (props) => {
 
   return (
     <div className="containerLogin">
-      <div className="logo"></div>
+      <div className="logo" />
       <div className="boxLogin">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="field">
@@ -65,7 +65,7 @@ const Login = (props) => {
               ref={register({ required: true, pattern: /^\S+@\S+$/i })}
             />
             {errors.email &&
-              errors.email.type === "required" &&
+              errors.email.type === 'required' &&
               errorMessage(required)}
 
             <input
@@ -78,7 +78,7 @@ const Login = (props) => {
               ref={register({ required: true })}
             />
             {errors.password &&
-              errors.password.type === "required" &&
+              errors.password.type === 'required' &&
               errorMessage(requiredPassword)}
             <div className="stay-connected-container">
               <div className="stay-connected">
