5426c8df2a5aa01c55482449cc6e10f6e6b42ef3
Pella0 <pellattiero.ju@gmail.com>
2021-01-05 14:01:47 +0100

    ajout ebauche filtre tag et articles

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,108 @@
 import React, { useState, useEffect } from 'react';
 import { getCollection } from '../services/API';
 
-const Feed = () => {
+const ListArticles = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [articlesFiltered, setArticlesFiltered] = useState([]);
+  const [tagList, setTagList] = useState([]);
+  const [allTags, setAllTags] = useState([]);
+
+  useEffect(() => {
+    getCollection('articles').then((elem) => {
+      setArticles(elem);
+    });
+  }, []);
+
+  useEffect(() => {
+    getCollection('tags').then((data) => setAllTags(data));
+  }, []);
+
+  useEffect(() => {
+    getCollection('tagToArticle').then((result) => {
+      const articleToFilter = result
+        .filter((article) => {
+          if (tagList.includes(article.tag_id)) {
+            return true;
+          }
+          return false;
+        })
+        .map((article) => {
+          return article.article_id;
+        });
+      setArticlesFiltered(articleToFilter);
+    });
+  }, [tagList]);
+
+  const handleTagList = (target) => {
+    if (tagList.includes(+target.id)) {
+      const newTagList = tagList.filter((item) => item !== +target.id);
+      setTagList(newTagList);
+    } else {
+      setTagList((prevState) => [...prevState, +target.id]);
+    }
+  };
+
+  return (
+    <div>
+      <div className="filterContainer">
+        {allTags &&
+          allTags.map((tag) => {
+            return (
+              <div key={tag.id}>
+                <button
+                  type="button"
+                  className="filterButton"
+                  id={tag.id}
+                  onClick={(e) => handleTagList(e.target)}
+                >
+                  {tag.name}
+                </button>
+              </div>
+            );
+          })}
+      </div>
+      <div className="articleListContainer">
+        {articlesFiltered.length > 0
+          ? articles
+              .filter((article) => {
+                if (articlesFiltered.includes(article.id)) {
+                  return true;
+                }
+                return false;
+              })
+              .map((e) => {
+                return (
+                  <div key={e.id} className="ArticlesRow">
+                    <div className="articlesInfos">
+                      <p>
+                        {e.title} {e.image} {e.text}
+                      </p>
+                    </div>
+                  </div>
+                );
+              })
+          : articles.map((e) => {
+              return (
+                <div key={e.id} className="ArticlesRow">
+                  <div className="articlesInfos">
+                    <p>
+                      {e.title} {e.image} {e.text}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+      </div>
+    </div>
+  );
+};
+export default ListArticles;
+
+/* const Feed = () => {
   const [articles, setArticles] = useState([]);
   const [articlesFiltered, setArticlesFiltered] = useState([]);
 
+/* filtrer appartion des articles 
   useEffect(() => {
     getCollection('articles').then((data) => setArticles(data));
   }, []);
@@ -45,4 +143,4 @@ const Feed = () => {
   );
 };
 
-export default Feed;
+export default Feed; */
