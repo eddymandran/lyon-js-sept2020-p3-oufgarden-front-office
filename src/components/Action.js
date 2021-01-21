b72ef0b2c047c346577d08382470d0320a77f7a1
Pella0 <pellattiero.ju@gmail.com>
2021-01-21 14:10:48 +0100

    envoi des actions lors de la creation

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,87 +1,68 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useState } from 'react';
-import _ from 'lodash';
 import './style/Action.scss';
 import { useForm, Controller } from 'react-hook-form';
 import Select from 'react-select';
 import { getCollection, makeEntityAdder } from '../services/API';
 
 const Action = (props) => {
-  // eslint-disable-next-line react/destructuring-assignment
-  const { handleSubmit, control } = useForm();
-  const [gardenAction, setgardenAction] = useState([]);
-  const [inputList, setInputList] = useState([
-    {
-      date: '',
-      time: '',
-      description: '',
+  const {
+    match: {
+      params: { id },
     },
-  ]);
+  } = props;
+
+  const { register, handleSubmit, control } = useForm();
+  const [gardenAction, setgardenAction] = useState([]);
+  const [gardenZone, setgardenZone] = useState([]);
 
   useEffect(() => {
     getCollection('actions').then((elem) => {
       setgardenAction(elem);
-      console.log(elem);
     });
   }, []);
 
-  const options = gardenAction.map((elem) => {
+  useEffect(() => {
+    getCollection(`garden/${id}/zones`).then((elem) => {
+      setgardenZone(elem);
+      console.log(gardenZone);
+    });
+  }, []);
+
+  const gardenOptions = gardenAction.map((elem) => {
     return {
       value: elem.id,
-      label: elem.name,
+      label: `${elem.name}`,
     };
   });
-  const handleInputChange = (e, index) => {
-    const { name, value } = e.target;
-    const deepCopyList = _.cloneDeep(inputList);
-
-    deepCopyList[index][name] = value;
-    setInputList(deepCopyList);
-  };
-
-  const handleAddClick = () => {
-    const deepCopyList = _.cloneDeep(inputList);
-    deepCopyList.push({
-      date: '',
-      time: '',
-      description: '',
-    });
-    setInputList(deepCopyList);
-  };
-
-  /*   const onSubmit = async (data) => {
+  const zoneOption = gardenZone.map((elem) => {
+    return {
+      value: elem.id,
+      label: `${elem.name}`,
+    };
+  });
+  const onSubmit = async (data, e) => {
     const newData = {
-      date: data.date,
-      time: data.time,
-      description: data.description,
       ...data,
+      action_id: data.action.value,
+      zone_id: data.zone.value,
     };
-    console.log(newData);
-    await makeEntityAdder('action')(newData);
-    setInputList([]);
-  }; 
- */
-  const onSubmit = (data, e) => {
-    const newData = {
-      date: data.date,
-      time: data.time,
-      description: data.description,
-    };
-    console.log(data);
 
-    makeEntityAdder('action')(newData)
-      .catch((err) => console.log(err.response.data))
-      .then(() => {
-        e.target.reset();
-        setInputList([
-          {
-            date: '',
-            time: '',
-            description: '',
-          },
-        ]);
-      })
-      .then(() => props.history.push('/garden'));
+    try {
+      await makeEntityAdder(`garden/${id}/zones/${data.zone.value}/actionFeed`)(
+        newData
+      )
+        .then(() => {
+          setgardenAction([]);
+          setgardenZone([]);
+        })
+        .then(() => {
+          props.history.push('/garden');
+        });
+    } catch (err) {
+      console.log(err);
+    }
+    e.target.reset();
   };
 
   return (
@@ -90,74 +71,45 @@ const Action = (props) => {
       <div key={gardenAction.id} className="action-row">
         <form className="formContainer" onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="actionGarden">
-            {inputList.map((x, i) => {
-              return (
-                // not the best solution for the key but could not find another one - do not replace with Math.random()
-                <div className="actionGarden" key={inputList.id}>
-                  <Controller
-                    name="action"
-                    control={control}
-                    defaultValue=""
-                    render={({ onChange, value }) => (
-                      <Select
-                        options={options}
-                        onChange={(e) => {
-                          onChange(e);
-                        }}
-                        value={value}
-                        isMulti
-                      />
-                    )}
-                  />
-                  <input
-                    className="date"
-                    name="date"
-                    type="date"
-                    placeholder="date d'action"
-                    value={x.date}
-                    onChange={(e) => handleInputChange(e, i)}
-                  />
-                  <input
-                    className="time"
-                    name="time"
-                    type="time"
-                    placeholder="time"
-                    value={x.time}
-                    onChange={(e) => handleInputChange(e, i)}
-                  />
-                  <div className="inputGroup">
-                    <input id="radio1" name="radio" type="radio" />
-                    <label htmlFor="radio1" />
-                  </div>
-                  <textarea
-                    className="description"
-                    name="description"
-                    type="text"
-                    placeholder="Description de la zone"
-                    value={x.description}
-                    onChange={(e) => handleInputChange(e, i)}
-                  />
-                  <div className="btn-box">
-                    {inputList.length - 1 === i && (
-                      <button
-                        className="btnBox"
-                        type="button"
-                        onClick={handleAddClick}
-                      >
-                        Ajouter
-                      </button>
-                    )}
-                  </div>
-                </div>
-              );
-            })}
+            <div className="actionGarden" key={gardenAction.id}>
+              <Controller
+                as={Select}
+                options={gardenOptions}
+                name="action"
+                isClearable
+                control={control}
+              />
+              <input
+                className="date"
+                name="date"
+                type="date"
+                placeholder="date d'action"
+                ref={register}
+              />
+              <input
+                className="time"
+                name="time"
+                type="time"
+                placeholder="time"
+                ref={register}
+              />
+              <Controller
+                as={Select}
+                options={zoneOption}
+                name="zone"
+                isClearable
+                control={control}
+              />
+              <textarea
+                className="description"
+                name="description"
+                type="text"
+                placeholder="Description de la zone"
+                ref={register}
+              />
+            </div>
           </label>
-          <button
-            type="button"
-            className="sendButton" /* onClick={handleCreate} */
-          >
-            Créer
-          </button>
+          <input type="submit" className="sendButton" value="Creer" />
         </form>
       </div>
     </div>
