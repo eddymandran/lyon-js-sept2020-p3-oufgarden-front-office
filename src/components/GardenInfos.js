13fd234e7efebf4da81e768c4c19326e28ac1d5c
Pella0 <pellattiero.ju@gmail.com>
2021-01-22 12:56:40 +0100

    css infos garden optimisé

--- src/components/GardenInfos.js
+++ src/components/GardenInfos.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './style/GardenInfos.scss';
+import { Link } from 'react-router-dom';
 import { getEntity } from '../services/API';
 
 const GardenInfos = (props) => {
@@ -15,13 +16,23 @@ const GardenInfos = (props) => {
   }, []);
 
   return (
-    <div className="garden-list-container">
-      <div key={gardenInfos.id} className="garden-row">
-        <p>{gardenInfos.name}</p>
-        <p>{gardenInfos.descrption}</p>
-        <p>{gardenInfos.exposition}</p>
-        <p>{gardenInfos.address}</p>
-        <img className="imgGarden" src={gardenInfos.url} alt="jardin" />
+    <div className="garden-list-container-infos">
+      <div key={gardenInfos.id} className="garden-row-infos">
+        <div className="whitebar-infos">
+          <div className="back-home-infos">
+            <Link className="link-back-feed-garden" to="/garden">
+              <div className="back-arrow-infos" />
+              <div className="retour-infos">Retour</div>
+            </Link>
+          </div>
+        </div>
+        <img className="imgGarden" src={gardenInfos.picture} alt="jardin" />
+        <div className="detailsGarden">
+          <h3>{gardenInfos.name}</h3>
+          <p>{gardenInfos.description}</p>
+          <p>Exposition : {gardenInfos.exposition}</p>
+          <p>Adresse : {gardenInfos.address}</p>
+        </div>
       </div>
     </div>
   );
