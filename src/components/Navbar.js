a3f0f48f876edc0ffbc6201aafac2de8ec4a2428
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-25 14:23:12 +0100

    start routing the calendar on the front

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style/Navbar.scss';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import history from '../history';
+import { getCollection } from '../services/API';
 
 const Navbar = () => {
+  const [myGarden, setMyGarden] = useState([]);
+  useEffect(() => {
+    getCollection('garden').then((elem) => {
+      setMyGarden(elem);
+    });
+  }, []);
   if (history.location.pathname === '/') {
     return false;
   }
+  console.log(myGarden);
+
   return (
     <div className="menu">
       <ul>
         <Link to="/articles">
           <div className="articles" />
         </Link>
-        <Link to="/calendar">
+        <Link to={`garden/${myGarden.id}/calendar`}>
           <div className="calendar" />
         </Link>
         <Link to="/garden">
