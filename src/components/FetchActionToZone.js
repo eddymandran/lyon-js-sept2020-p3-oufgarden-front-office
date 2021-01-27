8049dcc9e281c3c074059a5870868f85a443aa96
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-27 14:53:56 +0100

    wrong branch

---
+++ src/components/FetchActionToZone.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { getCollection } from '../services/API';
+
+const FetchActionToZones = ({ gardenId, gardenZone }) => {
+  const [actionToFeed, setActionToFeed] = useState([]);
+  /*   const [actionToFeedWithHour, setActionToFeedWithHour] = useState([]);
+   */
+  useEffect(() => {
+    getCollection(`garden/${gardenId}/zones/${gardenZone}/actionFeed`).then(
+      (data) => {
+        setActionToFeed(data);
+      }
+    );
+  }, []);
+  console.log(actionToFeed);
+
+  return (
+    <div>
+      {actionToFeed.map((action) => {
+        return (
+          <div className="rowsWithActionsFromZone">
+            <p>{action.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default FetchActionToZones;
