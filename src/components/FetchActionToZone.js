c3bdaa6750651a92620e3b46a1e593ad48c98985
AntoineGGG <a.gorenflot@gmx.fr>
2021-01-27 15:27:22 +0100

    full date fetching ok

--- src/components/FetchActionToZone.js
+++ src/components/FetchActionToZone.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+
 import { getCollection } from '../services/API';
 
 const FetchActionToZones = ({ gardenId, gardenZone }) => {
   const [actionToFeed, setActionToFeed] = useState([]);
+  const [actionToFeedWithFullDate, setActionToFeedWithFullDate] = useState([]);
 
   useEffect(() => {
     getCollection(`garden/${gardenId}/zones/${gardenZone}/actionFeed`).then(
@@ -11,13 +13,29 @@ const FetchActionToZones = ({ gardenId, gardenZone }) => {
       }
     );
   }, []);
+  useEffect(() => {
+    if (actionToFeed.length > 0) {
+      setActionToFeedWithFullDate(
+        actionToFeed.map((elem) => {
+          return {
+            ...elem,
+            fullDate: `${elem.date.split('T')[0]} ${elem.time}`,
+          };
+        })
+      );
+    }
+  }, [actionToFeed]);
   console.log(actionToFeed);
+  console.log(actionToFeedWithFullDate);
   return (
     <div>
-      {actionToFeed.map((action) => {
+      {actionToFeedWithFullDate.map((action) => {
         return (
           <div className="rowsWithActionsFromZone">
-            <p>{action.description}</p>
+            <p>
+              {action.name}: {action.firstname} {action.lastname}{' '}
+              {action.fullDate}
+            </p>
           </div>
         );
       })}
