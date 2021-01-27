d6bac02ba568e6e98d53b892b407449d3e993bee
Pella0 <pellattiero.ju@gmail.com>
2021-01-27 16:24:33 +0100

    css garden infos/ nom des action en dessous des images

--- src/components/GardenInfos.js
+++ src/components/GardenInfos.js
@@ -60,7 +60,10 @@ const GardenInfos = (props) => {
           <h3>{gardenInfos.name}</h3>
           <p>{gardenInfos.description}</p>
           <p>Exposition : {gardenInfos.exposition}</p>
-          <p>Adresse : {gardenInfos.address_id}</p>
+          <p>
+            Adresse : {gardenInfos.street} {gardenInfos.zip_code}{' '}
+            {gardenInfos.city}
+          </p>
         </div>
 
         <div className="gardenActionContainer">
@@ -78,13 +81,17 @@ const GardenInfos = (props) => {
                     to={`/garden/${id}/action/${e.id}`}
                     className={`action-${e.name}`}
                   >
-                    <div
-                      key={e.id}
-                      type="image"
-                      alt="action"
-                      contentEditable="false"
-                      data-placeholder={e.name}
-                    />
+                    <div className="imageActionContainer">
+                      <div
+                        key={e.id}
+                        type="image"
+                        alt="action"
+                        contentEditable="false"
+                      />
+                      <div className="nameAction">
+                        <p>{e.name}</p>
+                      </div>
+                    </div>
                   </Link>
                 );
               })}
